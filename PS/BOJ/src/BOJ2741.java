6c3f5cd42ba5f94cfdb85d3a8f3ed636717e6e8e
jiwon <jiwon.swdev@gmail.com>
2019-03-13 16:27:50 +0900

    Add BOJ #2741

---
+++ PS/BOJ/src/BOJ2741.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ2741 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i=0; i < n; i++) {
+            System.out.println(i+1);
+        }
+    }
+}
