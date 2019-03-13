2446566c49f8e2f2194b7532ca541c86d1ee36ca
jiwon <jiwon.swdev@gmail.com>
2019-03-13 16:07:48 +0900

    Add BOJ #11719

---
+++ PS/BOJ/src/BOJ11719.java
@@ -0,0 +1,15 @@
+// BOJ 11718과 동일한 코드
+
+import java.util.Scanner;
+
+public class BOJ11719 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String text;
+
+        while(in.hasNextLine()) {
+            text = in.nextLine();
+            System.out.println(text);
+        }
+    }
+}
