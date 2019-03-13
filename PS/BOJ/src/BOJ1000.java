e2919c2187fdd4e38bc313ad81ae1a651a79c41b
jiwon <jiwon.swdev@gmail.com>
2019-03-13 13:06:17 +0900

    Add BOJ #1000

---
+++ PS/BOJ/src/BOJ1000.java
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/1000
+import java.util.Scanner;
+
+public class BOJ1000 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(a+b);
+    }
+}
--- PS/BOJ/src/BOJ2557.java
+++ PS/BOJ/src/BOJ2557.java
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/2557
+
 public class BOJ2557 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
