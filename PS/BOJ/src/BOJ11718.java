3f2ba4b77fb00c2254bfc0de27d05fb2f70d270c
jiwon <jiwon.swdev@gmail.com>
2019-03-13 16:05:45 +0900

    Add BOJ #11718

---
+++ PS/BOJ/src/BOJ11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ11718 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String text = "";
+
+        while(in.hasNextLine()) {
+            text = in.nextLine();
+            System.out.println(text);
+        }
+    }
+}
