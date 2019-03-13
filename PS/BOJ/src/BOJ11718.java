d99cee15d9247513236b4dc10438fd1e042a1516
jiwon <jiwon.swdev@gmail.com>
2019-03-13 16:08:02 +0900

    Update BOJ #11718

--- PS/BOJ/src/BOJ11718.java
+++ PS/BOJ/src/BOJ11718.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BOJ11718 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String text = "";
+        String text;
 
         while(in.hasNextLine()) {
             text = in.nextLine();
