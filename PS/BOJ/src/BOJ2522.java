98bc66ae0dbbcc73705d63a72666cd162064e227
jiwon <jiwon.swdev@gmail.com>
2019-03-15 16:27:51 +0900

    Add BOJ #2522

---
+++ PS/BOJ/src/BOJ2522.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BOJ2522 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
