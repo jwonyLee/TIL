d637d213154664f17e390db41817e8448409dc49
jiwon <jiwon.swdev@gmail.com>
2019-03-19 20:59:33 +0900

    Add SW Expert Academy #2047

---
+++ SWExpertAcademy/src/problem2047.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class problem2047 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+        System.out.println(text.toUpperCase());
+    }
+}
