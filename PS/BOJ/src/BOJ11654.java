9dd8b70eff2c0249918c2c796b86926690870db9
jiwon <jiwon.swdev@gmail.com>
2019-04-21 21:46:36 +0900

    Add BOJ #11654 아스키 코드

---
+++ PS/BOJ/src/BOJ11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BOJ11654 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        char val = in.nextLine().charAt(0);
+        System.out.println((int) val);
+    }
+}
