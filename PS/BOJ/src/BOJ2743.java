071583dabe154adb19c601cabe57c1964ac5378d
jiwon <jiwon.swdev@gmail.com>
2019-04-24 20:27:32 +0900

    Add BOJ #2743 단어 길이 재기

---
+++ PS/BOJ/src/BOJ2743.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BOJ2743 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+        System.out.println(text.length());
+    }
+}
