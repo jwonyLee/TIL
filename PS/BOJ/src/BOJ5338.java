ef4b1756b7bbe9c5e41ed1ed12c589bfcd0f6d8b
jiwon <jiwon.swdev@gmail.com>
2019-04-24 20:37:34 +0900

    Add BOJ #5338 마이크로소프트 로고

---
+++ PS/BOJ/src/BOJ5338.java
@@ -0,0 +1,9 @@
+public class BOJ5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
