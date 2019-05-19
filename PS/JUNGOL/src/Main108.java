60a55d7d4fe842b4d6b30997e654c4dd9904df9e
jiwon <jiwon.swdev@gmail.com>
2019-05-20 00:05:40 +0900

    Add JUNGOL 108 : 입력 - 형성평가3

---
+++ PS/JUNGOL/src/Main108.java
@@ -0,0 +1,8 @@
+public class Main108 {
+    public static void main(String[] args) {
+        int a = 50;
+        double b = 100.12;
+
+        System.out.println(String.format("%.2f * %d = %d", b, a, (int) (a * b)));
+    }
+}
