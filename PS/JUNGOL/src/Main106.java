bdb9d08438647e7ea3c3794ed4c1ca07578bf2ca
jiwon <jiwon.swdev@gmail.com>
2019-05-20 00:00:34 +0900

    Add JUNGOL 105 : 입력 - 형성평가1

---
+++ PS/JUNGOL/src/Main106.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main106 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = 10, b = 20, c = 30;
+        System.out.println(String.format("%d + %d = %d", a, b, c));
+    }
+}
