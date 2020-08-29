a93a0ad93292f80b5d9cf4a5f3b6b062df8ffbab
JiWon Lee <jiwon.swdev@gmail.com>
2020-08-29 13:21:04 +0900

    modify: BOJ #17087 - 숨바꼭질 6

--- Algorithms/BOJ/17087.py
+++ Algorithms/BOJ/17087.py
@@ -11,11 +11,8 @@ def gcd(a, b):
 n, s = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
-if n < 2:
-    print(abs(s - arr[0]))
-else:
-    g = abs(s - arr[0])
-    for i in range(1, len(arr)):
-        g = gcd(g, abs(s - arr[i]))
-    
-    print(g)
+g = abs(s - arr[0])
+for i in range(1, n):
+    g = gcd(g, abs(s - arr[i]))
+
+print(g)
