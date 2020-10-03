aa723dd07602b247415ebbca832af191c1bc7bdd
JiWon Lee <jiwon.swdev@gmail.com>
2020-10-03 20:48:19 +0900

    feat: LeetCode - Group Anagrams

---
+++ Algorithms/LeetCode/groupAnagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+            
+        return anagrams.values()
