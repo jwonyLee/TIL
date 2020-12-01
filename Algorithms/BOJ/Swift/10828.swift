8f9130b0090c3b41aa5c8dc86e2afc8c5a87a7f3
JiWon Lee <jiwon.swdev@gmail.com>
2020-12-01 14:44:30 +0900

    feat: BOJ #10828 - 스택 변경

--- Algorithms/BOJ/Swift/10828.swift
+++ Algorithms/BOJ/Swift/10828.swift
@@ -1,50 +1,20 @@
 // https://www.acmicpc.net/problem/10828
 // 스택
-var stack = Array(repeating: 0, count:  10000)
-var pos = 0
-
-func push(_ x: Int) {
-    stack[pos] = x
-    pos += 1
-}
-
-func pop() -> Int {
-    if pos < 1 { // 스택이 비어있으면
-        return -1
-    }
-    pos -= 1
-    return stack[pos]
-}
-
-func size() -> Int {
-    return pos
-}
-
-func top() -> Int {
-    if pos < 1 {
-        return -1
-    }
-    
-    return stack[pos-1]
-}
-
-func empty() -> Int {
-    return pos < 0 ? 1 : 0
-}
+var stack: [Int] = []
 
 for _ in 0..<Int(readLine()!)! {
     let op = readLine()!.split(separator: " ")
     switch op[0] {
     case "push":
-        push(Int(op[1])!)
+        stack.append(Int(op[1])!)
     case "pop":
-        print(pop())
+        print(stack.isEmpty ? -1 : stack.removeLast())
     case "size":
-        print(size())
+        print(stack.count)
     case "empty":
-        print(empty())
+        print(stack.isEmpty ? 1 : 0)
     case "top":
-        print(top())
+        print(stack.isEmpty ? -1 : stack.last!)
     default: break
     }
 }
